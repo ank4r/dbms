52f2bd46fa5e4182d8898aeb79a831e0c2dabc92
ravibansal <ravi220644@gmail.com>
2016-02-26 20:34:11 +0530

    databases for subtopics and topics and their relation among themselves and lecutures

--- login/init.php
+++ login/init.php
@@ -1,8 +1,12 @@
 <?php
 include('helper_modules.php');
-
-   createMembersTable();
-   //createLecturesTable();
+	//run the function call according to their priority wise i.e. low integer value = higher priority
+   //createMembersTable(); //1
+   //createLecturesTable(); //2
+	//createSubTopicTable(); //3
+	//createTopicTable(); //4
+	relationTopic_SubTopic();//5
+	relationSubTopic_Lecture();//6
 	exit();
 ?>
 
